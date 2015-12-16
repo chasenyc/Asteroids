634d2c4e5161729c62cc032eda5fa9260c167291
Alex Harris <chasenyc@gmail.com>
2015-12-16 00:33:25 -0500

    Add randomVel test.

--- jasmine/spec/UtilSpec.js
+++ jasmine/spec/UtilSpec.js
@@ -1,14 +1,20 @@
-describe("Util", function () {
-
+describe("Util#inherits", function () {
 
   it("should allow a class to be inherited", function () {
-    var ObjectOne = function () {
-      console.log('test');
-    };
+    var ObjectOne = function () {};
     ObjectOne.prototype.methodOne = function () {};
     var ObjectTwo = function () {};
     Asteroids.Util.inherits(ObjectTwo, ObjectOne);
     var newObjectTwo = new ObjectTwo ();
     expect(typeof newObjectTwo.methodOne).toBe("function");
+  });
+});
+
+
+describe("Util#randomVel", function () {
+  it("should return random velocities each time", function () {
+    var velOne = Asteroids.Util.randomVel(1,0);
+    var velTwo = Asteroids.Util.randomVel(1,0);
+    expect(velOne[0] !== velTwo[0] && velOne[1] !== velTwo[1]).toBe(true);
   });
 });
