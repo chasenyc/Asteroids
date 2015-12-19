57ec3a80cb7d782b10e115df866d395031956b34
Alex Harris <chasenyc@gmail.com>
2015-12-18 20:58:20 -0500

    Refactor code and implement movingObject beginning tests.

---
+++ jasmine/spec/MovingObjectSpec.js
@@ -0,0 +1,28 @@
+describe("movingObject", function () {
+  var objectOne = new Asteroids.MovingObject ();
+  objectOne.radius = 2;
+  objectOne.pos = [0,0];
+  objectOne.vel = [1,1];
+  var objectTwo = new Asteroids.MovingObject ();
+  objectTwo.radius = 2;
+  objectTwo.pos = [1,1];
+  var objectThree = new Asteroids.MovingObject ();
+  objectThree.radius = 2;
+  objectThree.pos = [5,0];
+
+  beforeEach(function() {
+
+  });
+
+  it("movingObject#isCollideWith returns correct result",
+  function () {
+    expect(objectOne.isCollideWith(objectTwo)).toBe(true);
+    expect(objectOne.isCollideWith(objectThree)).toBe(false);
+  });
+
+  it("movingObject#move moves the object the correct amount",
+  function () {
+    objectOne.move();
+    expect(objectOne.pos).toEqual([1,1]);
+  });
+});
--- jasmine/spec/UtilSpec.js
+++ jasmine/spec/UtilSpec.js
@@ -1,31 +1,30 @@
-describe("Util#inherits", function () {
+describe("Util", function () {
 
-  it("should allow a class to be inherited", function () {
-    var ObjectOne = function () {};
-    ObjectOne.prototype.methodOne = function () {};
-    var ObjectTwo = function () {};
-    Asteroids.Util.inherits(ObjectTwo, ObjectOne);
-    var newObjectTwo = new ObjectTwo ();
-    expect(typeof newObjectTwo.methodOne).toBe("function");
+  it("Util#inherits should allow a class to be inherited",
+    function () {
+      var ObjectOne = function () {};
+      ObjectOne.prototype.methodOne = function () {};
+      var ObjectTwo = function () {};
+      Asteroids.Util.inherits(ObjectTwo, ObjectOne);
+      var newObjectTwo = new ObjectTwo ();
+      expect(typeof newObjectTwo.methodOne).toBe("function");
   });
-});
 
-describe("Util#randomVel", function () {
-  it("should return random velocities each time", function () {
-    var velOne = Asteroids.Util.randomVel(1,0);
-    var velTwo = Asteroids.Util.randomVel(1,0);
-    expect(velOne[0] !== velTwo[0] && velOne[1] !== velTwo[1]).toBe(true);
+  it("Util#randomVel should return random velocities each time",
+    function () {
+      var velOne = Asteroids.Util.randomVel(1,0);
+      var velTwo = Asteroids.Util.randomVel(1,0);
+      expect(velOne[0] !== velTwo[0] && velOne[1] !== velTwo[1]).toBe(true);
   });
-});
 
-describe("Util#distance", function () {
-  it("should return correct distance between two coordinates", function () {
-    var posOne = [0,0];
-    var posTwo = [3,0];
-    var distance = Asteroids.Util.distance(posOne, posTwo);
-    var posThree = [0,0];
-    var posFour = [3,4];
-    var distanceTwo = Asteroids.Util.distance(posThree, posFour);
-    expect(distanceTwo).toBe(5);
+  it("Util#distance should return correct distance between two coordinates",
+    function () {
+      var posOne = [0,0];
+      var posTwo = [3,0];
+      var distance = Asteroids.Util.distance(posOne, posTwo);
+      var posThree = [0,0];
+      var posFour = [3,4];
+      var distanceTwo = Asteroids.Util.distance(posThree, posFour);
+      expect(distanceTwo).toBe(5);
   });
 });
